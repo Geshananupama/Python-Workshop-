bdbf72e0ce0fe68890095a1b82f5831d90dbd03c
unknown <239222@TECH.WYB.AC.LK>
2025-03-26 21:26:42 +0530

    first commit

---
+++ Q5.py
@@ -0,0 +1,22 @@
+num=[1,2,3,4,5,6,7,8,9,10,11,12]
+num.sort()
+# num.sort(reverse=True)
+# print (num)
+# num.append(456)
+# num.extend([50,60,70,80])
+# num.index(5)
+# print (num.index(5))
+# print (max(num))
+# print (min(num))
+# print (len(num))
+
+# def filter_value(cost):
+#     if(cost>350):
+#         return True
+#     else:
+#         return False
+
+samplelist["123","name",54,8,6,9,4,7,1,]
+samplelsit.clear(
+    print(samplelist)
+)
---
+++ array.py
@@ -0,0 +1,3 @@
+x=["Dileep","Lakshan",11,50,68]
+for i in x:
+    print(type[i])
